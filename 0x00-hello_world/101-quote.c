274c3f82e23ba6f1172b38d13d587d3f852135a5
Niyiadekola <you@niyiadekola04@gmail.com>
2022-09-09 09:36:15 -0700

    Task 8 level C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - Print a text
+ * main - Prints a text
  *
  * Return: Always 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar,
 2015-10-19\n", 59);
